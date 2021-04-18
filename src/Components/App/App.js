1282814046d9a1f598ced6e74e515e2334aeb87d
Tomas Padilla <padillatomasagustin@gmail.com>
2021-04-18 13:35:29 +0200

    Primo nacho

---
+++ src/Components/App/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Clean CRA with subfolders.</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// CSS:
+import "./CSS/index.css";
+// Components:
+import {App} from "./Components/App"
+
+ReactDOM.render(
+<App />
+  ,
+  document.getElementById('root')
+);
